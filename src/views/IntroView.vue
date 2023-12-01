8bac63eb2cdd735d3adf6683addce900624126d8
JarikiSungnyung <hyuk0496@gmail.com>
2023-12-01 09:46:40 +0900

    style: Update intro page

--- src/views/IntroView.vue
+++ src/views/IntroView.vue
@@ -2,11 +2,15 @@
     <div class="container">
         <div class="introbox">
             <h1>Reading Life</h1>
+            <h3>당신의 독서 경험을 나누고 싶은가요?</h3>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor
-                sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                그렇다면 ReadingLife가 바로 당신을 위한 공간입니다. <br />
+                ReadingLife는 책을 사랑하는 사람들이 자신의 독서 경험을 공유하고, 다른 사람들의 독서 경험을 통해 새로운 교양과 지식을 얻을 수 있는 온라인 커뮤니티입니다. 책을 통해 우리는 새로운 세상을 발견하고, 다양한 인생을 체험하며, 지식의
+                풍요로움을 느낄 수 있습니다. ReadingLife는 이러한 책 읽기의 가치를 극대화하고자 합니다. <br /><br />
+                ReadingLife에서는 당신이 읽은 책에 대한 감상평을 쓰고, 다른 사람들의 감상평을 읽을 수 있습니다. 또한, 자신이 읽은 책에 대한 전문적인 리뷰를 작성하거나, 다른 사람들의 리뷰를 통해 책에 대한 깊이 있는 이해를 할 수 있습니다. 또한, 각자의
+                독서 방식과 팁을 공유하며, 서로를 돕고 도움을 받는 커뮤니티를 형성하고 있습니다. <br /><br />
+                ReadingLife는 단순히 책을 읽는 것을 넘어서, 책을 통한 지식 공유와 교류를 추구합니다. 우리는 책이 단순히 개인의 취미나 지식 향상의 수단이 아니라, 사람들과의 연결을 통해 새로운 가치와 의미를 창출할 수 있는 도구라고 믿습니다.<br /><br />
+                ReadingLife와 함께 책을 통해 세상을 넓혀보세요. 책이 당신에게 주는 풍요로움을 다른 사람들과 나누고, 다른 사람들의 경험을 통해 또 다른 풍요로움을 발견하세요. ReadingLife에서 당신의 독서 경험을 풍요롭게 만드는 여정을 시작해보세요.
             </p>
         </div>
     </div>
